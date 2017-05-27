496516731a11387607d2c7fb77afd39d0a53092b
vbibikov <evilavenger@yandex.ru>
2017-05-27 15:56:41 +0300

    Service init

--- backend/HashtagAggregatorTwitterService/Program.cs
+++ backend/HashtagAggregatorTwitterService/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace HashtagAggregatorTwitterService
@@ -12,15 +7,15 @@ namespace HashtagAggregatorTwitterService
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false)
                 .Build();
 
-            host.Run();
-        }
     }
 }
